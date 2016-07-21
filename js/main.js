7bd1f785f9b53c41694de5536e2509435dec5521
Miguel Pasadinhas <miguel.pasadinhas@tecnico.ulisboa.pt>
2016-07-21 14:42:42 +0100

    p

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ function create_unit_button_dom(id, name) {
     var btn_class = (balance > 0) ? "default" : "danger"
     var btn_disabled = (balance > 0) ? "" : "disabled"
     var exclamation_glyph = (balance > 0) ? "" : create_glyphicon_button_label("exclamation-sign").wrap('<p/>').parent().html()
-
+    console.log(exclamation_glyph)
     return $('\
         <button data-unit-id="'+id+'" data-balance="'+balance+'" '+btn_disabled+' type="button" class="btn btn-labeled btn-'+btn_class+'"> \
             '+exclamation_glyph+'\
