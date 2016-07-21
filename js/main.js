84afe85cf6f27842080a5b3d12e17049b5212628
Miguel Pasadinhas <miguel.pasadinhas@tecnico.ulisboa.pt>
2016-07-21 14:21:34 +0100

    p

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ function create_unit_button_dom(id, name) {
 
     return $('\
         <button data-unit-id="'+id+'" '+btn_disabled+' type="button" class="btn btn-labeled btn-'+btn_class+'"> \
-            '+exclamation_glyph.wrap('<p/>').parent().html()+'\
+            '+(exclamation_glyph) ? exclamation_glyph.wrap('<p/>').parent().html() ? ""+'\
             <span class="btn-text">'+name+'</span> \
             <hr> \
             <span class="btn-text">Saldo: '+balance.toFixed(2)+'</span> \
